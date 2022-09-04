2502c10be8a032537aad95ce3c3885ea960f043a
Soufian Ben Afia <soufian.benafia@gmx.de>
2022-09-04 02:03:45 +0200

    further adjustments

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('goodies.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- goodies/models.py
+++ goodies/models.py
@@ -1,36 +1,54 @@
 from email.mime import image
 from django.db import models
 from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     categoryID = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=4, decimal_places=2)
-    image = models.ImageField()
+    image = ResizedImageField(size=[370, 350])
+
+    def __str__(self):
+        return self.name
 
 
 class ProductDetail(models.Model):
     productID = models.OneToOneField(
         Product, on_delete=models.CASCADE, primary_key=True)
     availability = models.IntegerField()
+    discriptionTitle = models.CharField(max_length=100, blank=True, null=True)
     discription = models.TextField()
+
+    def __str__(self):
+        return self.discriptionTitle
 
 
 class ProductDetailImage(models.Model):
     productID = models.ForeignKey(ProductDetail, on_delete=models.CASCADE)
     image = models.ImageField()
 
+    def __str__(self):
+        return self.id
+
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    username = models.CharField(
+        max_length=100, unique=True, blank=True, null=True)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.username
 
 
 class Order(models.Model):
@@ -39,6 +57,9 @@ class Order(models.Model):
     quantity = models.IntegerField()
     date_ordered = models.DateTimeField(auto_now_add=True, null=True)
     totalAmount = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.id
 
 
 class OrderItem(models.Model):
