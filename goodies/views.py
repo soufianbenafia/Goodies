5c0b06b22e31a116e82897932758ecc7e2ba4162
Soufian Ben Afia <soufian.benafia@gmx.de>
2022-09-05 00:09:24 +0200

    products and categories dynamic in shop page

--- goodies/views.py
+++ goodies/views.py
@@ -1,36 +1,49 @@
 from multiprocessing import context
 from django.shortcuts import render
 
+from goodies.models import Category, Product
+
 # Create your views here.
 
+
 def home(request):
-    context = {}
-    return render(request,'goodies/index.html',context)
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    context = {"products": products,"categories":categories}
+    return render(request, 'goodies/index.html', context)
+
 
 def about(request):
     context = {}
-    return render(request,'goodies/about.html',context)
+    return render(request, 'goodies/about.html', context)
+
 
 def cart(request):
     context = {}
-    return render(request,'goodies/cart.html',context)
+    return render(request, 'goodies/cart.html', context)
+
 
 def shop(request):
-    context = {}
-    return render(request,'goodies/shop.html',context)
-
-def contact(request):
-    context = {}
-    return render(request,'goodies/contact-us.html',context)
-
-def checkout(request):
-    context = {}
-    return render(request,'goodies/checkout.html',context)
-
-def account(request):
-    context = {}
-    return render(request,'goodies/my-account.html',context)
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    context = {"products": products,"categories":categories}
+    return render(request, 'goodies/shop.html', context)
 
 def shopDetail(request):
     context = {}
-    return render(request,'goodies/shop-detail.html',context)
+    return render(request, 'goodies/shop-detail.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'goodies/contact-us.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'goodies/checkout.html', context)
+
+
+def account(request):
+    context = {}
+    return render(request, 'goodies/my-account.html', context)
